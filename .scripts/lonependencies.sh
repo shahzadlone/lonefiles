d094d6dd4dd080561a79ea84d6b264b5add6023f
Shahzad Lone <shahzadlone@gmail.com>
2018-03-04 05:13:34 -0500

    Start writing a script called lonependencies.sh that will build all my linux package dependencies and show in the end if every main package got installed or not. Also want to have some extra packages downloaded after the main one to build on top of the main ones. For example after installing a main package vim, I would like an extra package vim-gnome to add on to vims functionality (for example enable x-clipboard).

---
+++ .scripts/lonependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#========================================================================================
+# Shahzad's script to install and build all the dependency packages. This script 
+#  can also be used to see if the main dependencies are installed on the system
+#  or not. Hence the name "lonependencies.sh" (dependencies/packages builder).
+#========================================================================================
