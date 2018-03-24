8df150c69d4a2306212b25cd403572f1a0d1aa8a
Shahzad Lone <shahzadlone@gmail.com>
2018-03-24 17:13:15 -0400

    Start the main wrapper script that will call all the other scripts.

--- .scripts/lonify.sh
+++ .scripts/lonify.sh
@@ -6,3 +6,6 @@
 #     2) Clone the dotfiles/lonefiles repository as a bare git repository, "lonestall.sh".
 #     3) Configure / compile / install all the cloned lonefiles/dotfiles, "lonefigure.sh".
 #========================================================================================
+
+# Address to where my script files in raw format are stored online.
+SCRIPTS_DIR='https://raw.githubusercontent.com/shahzadlone/lonefiles/master/.scripts/';
