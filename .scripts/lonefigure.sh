6d883849e83a3548df3863efa6efced16df636d6
Shahzad Lone <shahzadlone@gmail.com>
2018-03-24 17:06:23 -0400

    Finished configuring vim.

--- .scripts/lonefigure.sh
+++ .scripts/lonefigure.sh
@@ -5,3 +5,14 @@
 #  to my preference, and also change/configure any system specific preferences like
 #  for example if I want to remap any keys, or use my own defined keyboard shortcuts.
 #========================================================================================
+
+# ====================[ Configure vim ]====================
+
+# Install the plugins and quit all vim buffers that opened (can also do +qa).
+vim +PlugInstall +qall
+
+# Install/compile/configure the additional stuff for some plugins that require it.
+~/.vim/plug/YouCompleteMe/install.py --clang-completer
+
+# Just update the plugins and quit all vim buffers (probably don't need this step).
+vim +PlugUpdate +qall
