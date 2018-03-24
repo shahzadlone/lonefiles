a0b478de16f63c3aff2959f97a05011e391656d0
Shahzad Lone <shahzadlone@gmail.com>
2018-03-24 17:07:06 -0400

    Finish the part in script to configure terminal stuff.

--- .scripts/lonefigure.sh
+++ .scripts/lonefigure.sh
@@ -16,3 +16,17 @@ vim +PlugInstall +qall
 
 # Just update the plugins and quit all vim buffers (probably don't need this step).
 vim +PlugUpdate +qall
+
+# ====================[ Configure terminal ]====================
+
+# Clone the gnome terminal colors repository that will help us change our terminal theme.
+git clone https://github.com/metalelf0/gnome-terminal-colors.git ~/temp_terminal_theme;
+
+# Run the installation script and this is where we will interact with the script.
+source ~/temp_terminal_theme/install.sh;
+
+# After the user follows the on screen instructions and configuration is done, delete it.
+rm -rf ~/temp_terminal_theme;
+
+# Source the .bashrc so everything takes effect.
+source ~/.bashrc
