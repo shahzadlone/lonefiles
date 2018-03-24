0a514cd48b78c6e1c1805662d001db4a4d1a7179
Shahzad Lone <shahzadlone@gmail.com>
2018-03-24 17:08:18 -0400

    Start implementing the script that will clone the repository as a bare git repository. Make a helper called lit to make working with the bare repository easier.

--- .scripts/lonestall.sh
+++ .scripts/lonestall.sh
@@ -5,3 +5,11 @@
 #  git repository, and if there are any files that exist already that will conflict with
 #  the lonefiles, then backs them up in a hidden directory called ".lonefiles_backup".
 #========================================================================================
+
+# Clone the lonefiles as a bare git repository.
+git clone --bare https://github.com/shahzadlone/lonefiles ${HOME}/.lonefiles
+
+function lit {
+    \git --git-dir="${HOME}"/.lonefiles/ --work-tree="${HOME}" ${@};
+}
+
