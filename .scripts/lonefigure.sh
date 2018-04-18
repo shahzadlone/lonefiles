998b566f60038396a9a213140b7bc0ccbb1108fb
Shahzad Lone <shahzadlone@gmail.com>
2018-04-18 17:47:42 -0400

    Some permission bug fixes in the lonefigure(configuring script).

--- .scripts/lonefigure.sh
+++ .scripts/lonefigure.sh
@@ -9,24 +9,18 @@
 # ====================[ Configure vim ]====================
 
 # Install the plugins and quit all vim buffers that opened (can also do +qa).
-vim +PlugInstall +qall
+sudo vim +PlugInstall +qall
 
 # Install/compile/configure the additional stuff for some plugins that require it.
-~/.vim/plug/YouCompleteMe/install.py --clang-completer
+sudo ~/.vim/plug/YouCompleteMe/install.py --clang-completer
 
 # Just update the plugins and quit all vim buffers (probably don't need this step).
-vim +PlugUpdate +qall
+sudo vim +PlugUpdate +qall
 
 # ====================[ Configure terminal ]====================
 
-# Clone the gnome terminal colors repository that will help us change our terminal theme.
-git clone https://github.com/metalelf0/gnome-terminal-colors.git ~/temp_terminal_theme;
-
-# Run the installation script and this is where we will interact with the script.
-source ~/temp_terminal_theme/install.sh;
-
-# After the user follows the on screen instructions and configuration is done, delete it.
-rm -rf ~/temp_terminal_theme;
+# Install the gruv-box dark terminal theme from Mayccoll/Gogh.
+sudo wget -O - "https://git.io/v7eBS" | bash
 
 # Source the .bashrc so everything takes effect.
 source ~/.bashrc
